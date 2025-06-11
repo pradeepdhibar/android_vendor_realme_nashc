d2bc4c14b982ea38b121182fdfc24dc31d0582f7
MldKLr <mldklr@wolfdog.li>
2025-06-11 16:25:57 +0200

    Initial vendor dump

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),nashc)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,3402be32794ba8a056d8a58f141c42b38a91482c)
+$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,6df9e99d6f404ad321575f08811f53ca828cd03d)
+$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,cf0a16ac981be68178a20f843f072e73e52cdb30)
+$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,e57b6b16fe3b0671444fb5a22573db7e64ace20d)
+$(call add-radio-file-sha1-checked,radio/gz.img,81422984a9becfca09b98a83e901fa6e56dc9931)
+$(call add-radio-file-sha1-checked,radio/lk.img,84b2a40be01cd6ebce1c1b71744f220c85082dc8)
+$(call add-radio-file-sha1-checked,radio/logo.img,eb70e8b721692b7de446e611b1756946f775570b)
+$(call add-radio-file-sha1-checked,radio/md1img.img,3ec3dfe52e5c83f123050805c325a0d0b4d01344)
+$(call add-radio-file-sha1-checked,radio/preloader.img,10640feadfd954c3eb9edbca43a5dbacbcaf1ef0)
+$(call add-radio-file-sha1-checked,radio/scp.img,122e22b978be3d3571071d7a2d13ea79bafbc66b)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,2b11ec2c81a77139cd13cbb91416c38af4be15c7)
+$(call add-radio-file-sha1-checked,radio/sspm.img,72a07cbc391ed7f09fd1e3836bd3e0c0b555b262)
+$(call add-radio-file-sha1-checked,radio/tee.img,3d2bbd74a7fc3b667222590cc95064c97317c837)
+
+endif
